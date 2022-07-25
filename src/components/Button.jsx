d924e2f1241939574e9eef69d72ed5d778a2d537
Tatheer Mehdi <93590787+codethrx@users.noreply.github.com>
2022-07-25 13:58:44 +0500

    Button Story book done - story#1

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,27 +1,26 @@
-import React from "react";
 import PropTypes from "prop-types";
-function Button({ label, backgroundColor = "red", size = "md", onClick }) {
+
+function Button({ label, backgroundColor = "red", size = "md", handleClick }) {
   let scale = 1;
-  if (size === "sm") {
-    scale = 0.75;
-  }
-  if (size === "lg") {
-    scale = 1.5;
-  }
+  if (size === "sm") scale = 0.75;
+  if (size === "lg") scale = 1.5;
   const style = {
     backgroundColor,
     padding: `${scale * 0.5}rem ${scale * 1}rem`,
+    border: "none",
   };
   return (
-    <button style={style} onClick={onClick}>
+    <button onClick={handleClick} style={style}>
       {label}
     </button>
   );
 }
-Button.prototype = {
+
+Button.propTypes = {
   label: PropTypes.string,
   backgroundColor: PropTypes.string,
-  onClick: PropTypes.func,
   size: PropTypes.oneOf(["sm", "md", "lg"]),
+  handleClick: PropTypes.func,
 };
+
 export default Button;
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -3,15 +3,19 @@ import Button from "../components/Button";
 export default {
   title: "Button",
   component: Button,
-  argTypes: {
-    handleClick: { action: "Handle Clicked" },
-  },
+  argTypes: { handleClick: { action: "Click..." } },
 };
-// export const Red = () => <Button label="Press Red" backgroundColor="red" />;
 const Template = (args) => <Button {...args} />;
 export const Red = Template.bind({});
 Red.args = {
   backgroundColor: "red",
-  size: "sm",
-  label: "red",
+  size: "md",
+  label: "Red",
+};
+export const OliveLargeText = Template.bind({});
+OliveLargeText.args = {
+  backgroundColor: "olive",
+  size: "lg",
+  label:
+    "Infinity.................................................................",
 };
